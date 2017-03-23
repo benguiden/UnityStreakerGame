79a5db749354d864146a316be0996426d8bebc46
Ben Guiden <benguiden@hotmail.co.uk>
2017-03-23 19:19:12 +0000

    Initalize project

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	//This script rotates the game object's transform with the 'Mouse X' and the Mouse Y' axis inputs.
+
+}
