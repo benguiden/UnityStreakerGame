7e9c1632fc18934070a48d585dcad12f477eb750
Ben Guiden <benguiden@hotmail.co.uk>
2017-03-31 19:07:45 +0100

    Guards now chase and face players

---
+++ Assets/Scripts/NPCs/GuardAI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (CharacterController))]
+public class GuardAI : MonoBehaviour {
+	//AI for the guard with state machine, this script will be disabled when the guard jumps
+	//for the player and go into ragdoll until they get up off the ground afterwards.
+
+	//Variables
+	[Tooltip("Maximum running speed of the rigidbody.")]
+	public float maxSpeed;
+	[Tooltip("The time it takes to accelerate to maximum running speed from 0m/s.")]
+	public float accelTime;
+	[Tooltip("How fast the controller steers towards the target.")]
+	public float steeringSpd = 1f; //Should generally be close to 1, otherwise the AI is likely to either overshoot the player when chasing them, or steer unrealistically
+
+	private float speed, acceleration;
+
+	//Objects
+	private CharacterController controller;
+	private Transform target;
+
+	void Start(){
+		//Variables
+		acceleration = maxSpeed / accelTime; //This is similar to calculating speed using distance/time
+
+		//Objects
+		controller = this.GetComponent<CharacterController>();
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	void Update(){
+
+		//Accelerate
+		if (speed < maxSpeed) {
+			speed += acceleration * Time.deltaTime;
+		} else if (speed > maxSpeed) {
+			speed = maxSpeed; //Stop Accelerating
+		}
+
+		//Chase and Steer towards target
+		Vector3 move = NPC.ChaseForce(this.transform.position, target.position, speed, steeringSpd, controller.velocity * Time.deltaTime);
+
+		//Move controller
+		controller.SimpleMove (move);
+
+		//Rotate Object towards velocity
+		if (controller.velocity.normalized != Vector3.zero) //If the character is no moving, we don't want to set their rotation to snap to (0, 0, 0);
+			this.transform.rotation = Quaternion.LookRotation(controller.velocity.normalized);
+
+	}
+
+}
---
+++ Assets/Scripts/NPCs/NPC.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+	//This class will house different static functions that most AIs will use, since each AI will behave
+	//differently but still use these basic functions, we can have each AI have their own script and state machine
+	//but use static functions from here
+
+	//Seek Function
+	public static Vector3 ChaseForce(Vector3 source, Vector3 target, float moveSpd, float steeringSpd, Vector3 currentVelocity){
+		Vector3 dirToTarget = Vector3.Normalize (target - source);
+		Vector3 velToTarget = moveSpd * dirToTarget; //The steering speed determines how quickly the rigidbody gets to it's desired directional velocity.
+		return steeringSpd * (velToTarget - currentVelocity); //If the steering speed is under 1, the rigidbody is most likely to overshoot it's target.
+	}
+}
