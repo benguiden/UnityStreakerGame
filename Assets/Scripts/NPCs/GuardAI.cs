e85193cd926e067d39b9cd57c687e120a3664604
Ben Guiden <benguiden@hotmail.co.uk>
2017-04-07 13:15:12 +0100

    Added ragdoll physics to the guard.

--- Assets/Scripts/NPCs/GuardAI.cs
+++ Assets/Scripts/NPCs/GuardAI.cs
@@ -14,14 +14,19 @@ public class GuardAI : MonoBehaviour {
 	public float accelTime;
 	[Tooltip("How fast the controller steers towards the target.")]
 	public float steeringSpd = 1f; //Should generally be close to 1, otherwise the AI is likely to either overshoot the player when chasing them, or steer unrealistically
+	[Tooltip("The distance from the target that the controller desides to dive towards the target.")]
+	public float diveDistance = 5f;
+	[Tooltip("How long it takes for the character to recover from ragdoll mode.")]
+	public float recoverTime = 2f;
 
-	private float speed, acceleration;
+	private float speed, acceleration, recoverCount/*The counter to recover time*/;
 	private string state = "chase"; //This is the state the AI is in and determines how it will act 
 
 	//Objects
 	private CharacterController controller;
 	private Transform target;
 	private Animator anm;
+	private Rigidbody[] ragdollRBs;
 
 	void Start(){
 		//Variables
@@ -31,6 +36,7 @@ public class GuardAI : MonoBehaviour {
 		controller = this.GetComponent<CharacterController>();
 		target = GameObject.FindGameObjectWithTag("Player").transform;
 		anm = this.GetComponentInChildren<Animator>();
+		ragdollRBs = this.GetComponentsInChildren<Rigidbody> ();
 	}
 
 	void Update(){
@@ -59,7 +65,7 @@ public class GuardAI : MonoBehaviour {
 				this.transform.rotation = Quaternion.LookRotation (controller.velocity.normalized);
 
 			//Change state if close enough to target
-			if (Vector3.Distance (this.transform.position, target.position) <= 10f)
+			if (Vector3.Distance (this.transform.position, target.position) <= diveDistance)
 				state = "dive";
 
 			//Set Animation state
@@ -73,12 +79,22 @@ public class GuardAI : MonoBehaviour {
 			////////////////////////////////////////////////
 
 			//Move controller forward
-			controller.SimpleMove (this.transform.forward * maxSpeed);
+			controller.SimpleMove (this.transform.forward * maxSpeed * 1.2f);
 
 			//Check if animation is over
-			if (anm.GetCurrentAnimatorStateInfo (0).normalizedTime >= 1f) { //NormalizedTime returns the current state time / the animation length, so if this is > 1, the animation is over
+			AnimatorStateInfo anmState = anm.GetCurrentAnimatorStateInfo (0);
+			if ((anmState.normalizedTime >= 1f) && (anmState.IsName("Dive"))) { //NormalizedTime returns the current state time / the animation length, so if this is > 1, the animation is over
 				//Change state to ragdoll
-				state = "chase";
+				state = "ragdoll";
+				recoverCount = recoverTime;
+				//Turn on rigidbodies to ragdoll
+				foreach (Rigidbody rb in ragdollRBs) {
+					rb.isKinematic = false;
+					rb.velocity = controller.velocity * Time.deltaTime;
+					//rb.AddForce (this.transform.forward * maxSpeed * 1.2f, ForceMode.VelocityChange);
+				}
+				//Disable animator to let bones only be effected by the rigidbodies
+				anm.enabled = false;
 			}
 
 			//Set Animation state
@@ -90,6 +106,34 @@ public class GuardAI : MonoBehaviour {
 		//Ragdolling and falling to the ground, and staying on the ground for a specified ammount of time
 		case "ragdoll":
 			////////////////////////////////////////////////
+
+			//Set Animation state
+			anm.SetInteger ("state", 2);
+
+			//Count down recover time
+			recoverCount -= Time.deltaTime;
+
+			//Check if recovered
+			if (recoverCount <= 0f) {
+				//'Disable' rigidbodies
+				foreach (Rigidbody rb in ragdollRBs) {
+					rb.isKinematic = true;
+				}
+
+				//Enable animator
+				anm.enabled = true;
+
+				//Translate the Guard parent of the rigidbodies to the position of the 'main' bone
+				Vector3 bonePos = ragdollRBs[0].gameObject.transform.position; //We do this because when the rigidbodies are active (not kinematic), the children objects will translate away from the main parent object,
+				this.transform.position = new Vector3 (bonePos.x, this.transform.position.y, bonePos.z); //while the main parent object stays in place. So when we start using the animator again, the rendering mesh will
+																				//will seem to teleport back to the parent object, instead of where the rigidbodies appeared to stop on the ground.
+																				//That explanation may sound confusing, but you can comment out these lines to see what I'm on about.
+
+				//Change state
+				state = "chase";
+
+			}
+
 			break;
 			////////////////////////////////////////////////
 
