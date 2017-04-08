4c4416a148d6cd2e7524606405ed336495443a75
Ben Guiden <benguiden@hotmail.co.uk>
2017-04-08 21:09:52 +0100

    Added TVCamera script and object.

---
+++ Assets/Scripts/Environment/TVCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVCamera : MonoBehaviour {
+
+    [Tooltip("The smoothness of the camera rotation as it follows the player. 1 being too smooth that the camera doesn't actually rotate.")]
+    [Range(0f, 1f)]
+    public float smoothness;
+
+    private Transform target;
+
+    void Start(){
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update(){
+        //Look at the target
+        this.transform.LookAt(Vector3.Lerp(this.transform.forward, target.position, 1f - smoothness));
+    }
+}
