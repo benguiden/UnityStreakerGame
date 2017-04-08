5491c6d69426ba2f4a4d185e14ef6fa08c311016
Ben Guiden <benguiden@hotmail.co.uk>
2017-04-08 16:26:38 +0100

    Added Facing Angle function.

---
+++ Assets/Scripts/Maths/Geometry.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Geometry : MonoBehaviour {
+	//This class will house some of the static mathematical functions we need for the AI's
+
+	//This function will return the angle between the target position and the current object's FACING (localEularAngle.y) position
+	//This will be useful for FOV detection, or to see if the player is facing any NPCs
+	public static float FacingAngle(Transform thisTrans, Transform targetTrans){
+		Vector2 thisPos = new Vector2 (thisTrans.position.x, thisTrans.position.z);
+		Vector2 targetPos = new Vector2 (targetTrans.position.x, targetTrans.position.z);
+		float theta = Mathf.Atan2 (targetPos.y - thisPos.y, targetPos.x - thisPos.x) * Mathf.Rad2Deg;
+		return theta + thisTrans.localEulerAngles.y - 90f;
+	}
+}
