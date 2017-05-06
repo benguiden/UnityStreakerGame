b08a1c23ea9f3521781f3d4f3ba7c1bb4856aed3
Ben Guiden <benguiden@hotmail.co.uk>
2017-05-06 13:07:26 +0100

    Added ball kicking sound.

---
+++ Assets/Scripts/Audio/KickBallAudio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickBallAudio : MonoBehaviour {
+
+	[Tooltip("The acceleration at which the ball triggers the audio source to play.")]
+	public float accelerationTrigger;
+
+	private AudioSource audioSource;
+	private Rigidbody rb;
+	private float veloLast = 0f;
+
+	void Start(){
+		audioSource = this.GetComponent<AudioSource> ();
+		rb = this.GetComponent<Rigidbody> ();
+	}
+
+	void Update(){
+
+		//Get Acceleration
+		float acceleration = rb.velocity.magnitude - veloLast;
+		veloLast = rb.velocity.magnitude;
+
+		if (Mathf.Abs(acceleration) >= accelerationTrigger) {
+			audioSource.time = 0f;
+			audioSource.Play ();
+		}
+
+	}
+
+}
